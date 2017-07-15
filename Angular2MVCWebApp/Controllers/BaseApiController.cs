48773391746f82d917d21a9406fa52ef4609a896
Fernando Gomez <fernando.gmz12@gmail.com>
2017-07-15 13:29:36 -0300

    Avance hasta el punto 75

---
+++ Angular2MVCWebApp/Controllers/BaseApiController.cs
@@ -0,0 +1,21 @@
+﻿namespace Angular2MVCWebApp.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Web.Http;
+    using Newtonsoft.Json;
+    using Angular2MVCWebApp.DBContext;
+
+    public class BaseApiController : ApiController
+    {
+        protected readonly DatabaseEntities DatabaseEntities = new DatabaseEntities();
+
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ Angular2MVCWebApp/Controllers/UserAPIController.cs
@@ -0,0 +1,35 @@
+﻿namespace Angular2MVCWebApp.Controllers
+{
+    using System.Linq;
+    using System.Net.Http;
+    using System.Web.Http;
+    using System.Data.Entity;
+    using Angular2MVCWebApp.DBContext;
+
+    public class UserApiController : BaseApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(DatabaseEntities.User.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]User user)
+        {
+            DatabaseEntities.User.Add(user);
+            return ToJson(DatabaseEntities.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]User user)
+        {
+            DatabaseEntities.Entry(user).State = EntityState.Modified;
+            return ToJson(DatabaseEntities.SaveChanges());
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            User user = DatabaseEntities.User.FirstOrDefault(u => u.Id == id);
+            DatabaseEntities.User.Remove(user);
+            return ToJson(DatabaseEntities.SaveChanges());
+        }
+    }
+}
